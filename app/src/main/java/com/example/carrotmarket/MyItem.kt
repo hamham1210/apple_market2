95621b99d195cd4c09aee49231b23d57b1c286a5
hamham1210 <thwn1012@gmail.com>
2023-08-29 22:49:24 +0900

    first

---
+++ app/src/main/java/com/example/carrotmarket/MyItem.kt
@@ -0,0 +1,5 @@
+package com.example.carrot_market
+
+data class MyItem(val icon:Int,  val name:String,val ItemInfo:String,val nickname:String,val pirce:Int, val adress:String, val comment:Int, val heart:Int) {
+
+}
